4fe299a8cbd8bd1d8b7a314569b1fe8ace43f9a7
zuza <zuzanna.kulej@ge.com>
2018-07-18 14:57:00 +0200

    ngModel

--- src/app/playlists/playlists.module.ts
+++ src/app/playlists/playlists.module.ts
@@ -4,10 +4,12 @@ import { PlaylistsComponent } from './playlists/playlists.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { ListItemComponent } from './list-item/list-item.component';
 import { PlaylistDetailsComponent } from './playlist-details/playlist-details.component';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [ 
     PlaylistsComponent, 
